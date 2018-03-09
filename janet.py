4fd1c2f634a9efb29ea9c95408581ed04d7891ad
Sagar <sathakore@attain.com>
2018-03-09 13:59:14 -0500

    Remove exposed key and read from csv file

--- janet.py
+++ janet.py
@@ -1,6 +1,7 @@
 import telepot
 import sys
 import time
+import csv
 from telepot.loop import MessageLoop
 
 
@@ -9,8 +10,13 @@ def sendNotification(message):
     for line in f:
         bot.sendMessage(line, message)
 
+keys = {}
+with open('keys.csv', newline='') as keystore:
+    reader = csv.reader(keystore)
+    next(reader)
+    keys = dict(reader)
 
-bot = telepot.Bot('494856102:AAHCA__TrYFx6RjziauVINutEbdewrmHhDk')
+bot = telepot.Bot(keys['jake'])
 
 if sys.argv[1] == "message" :
     sendNotification(sys.argv[2])

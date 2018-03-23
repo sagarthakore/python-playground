16d3926b3fd6883e60f33985aa9f1e023dd85cd8
Sagar <sathakore@attain.com>
2018-03-23 13:34:21 -0400

    Make files ready for AWS

--- julian.py
+++ julian.py
@@ -5,14 +5,6 @@ import urllib3
 import csv
 from telepot.loop import MessageLoop
 
-# # You can leave this bit out if you're using a paid PythonAnywhere account
-proxy_url = "http://proxy.server:3128"
-telepot.api._pools = {
-    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
-}
-telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
-# end of the stuff that's only needed for free accounts
-
 keys = {}
 with open('keys.csv', newline='') as keystore:
     reader = csv.reader(keystore)

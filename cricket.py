72c80c7f86c375fdf7883389906b544b2815e7bd
Sagar <sathakore@attain.com>
2018-03-28 15:20:55 -0400

    Show results of previous matches

--- cricket.py
+++ cricket.py
@@ -4,7 +4,12 @@ import json
 c = Cricbuzz()
 matches = c.matches()
 
+
 for match in matches:
-    print(match['srs'] + " - " + match['status'])
-
-
+    if (match['mchstate'] == 'Result'):
+        data = c.livescore(match['id'])
+        print(data['matchinfo']['srs'] + " - " + data['matchinfo']['status'])
+        print(data['batting']['team'] + " - " + data['batting']['score'][0]['runs'] + "/" + data['batting']['score'][0]['wickets'] + " - " + data['batting']['score'][0]['overs'] + " over(s)")
+        print(data['bowling']['team'] + " - " + data['bowling']['score'][0]['runs'] + "/" + data['bowling']['score'][0]['wickets'] + " - " + data['bowling']['score'][0]['overs'] + " over(s)")
+    if(match['mchstate'] == 'nextlive'):
+        print(match['srs'] + " - " + match['status'])

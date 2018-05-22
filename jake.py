891a301bb3a72f072d3b429d10bf9e2ee58b20c5
Sagar <sagart12@gmail.com>
2018-05-21 20:48:11 -0400

    Geocode format address functionality

--- jake.py
+++ jake.py
@@ -1,43 +1,106 @@
 import time
 import random
-import datetime
 import telepot
 import os
 import subprocess
 import csv
 import mmap
 import urllib3
+import pyodbc
+import datetime
+import googlemaps
 from telepot.loop import MessageLoop
 from requests import get
 
+# Get connection string
+keys = {}
+with open('connection.csv', newline='') as keystore:
+    reader = csv.reader(keystore)
+    next(reader)
+    keys = dict(reader)
+connection_string = keys['pydb']
+
+# Establish connection to the database
+main_connection = pyodbc.connect(connection_string)
+main_cursor = main_connection.cursor()
+
+# Execute SQL command
+SQLCommand = ("select * from conf_keys where application = 'gmaps'")
+main_cursor.execute(SQLCommand)
+results = main_cursor.fetchone() 
+
+# Apply the key to gmaps
+gmaps = googlemaps.Client(key=str(results.key))
+
+# Execute SQL command
+SQLCommand = ("select * from conf_keys where application = 'jake'")
+main_cursor.execute(SQLCommand)
+results = main_cursor.fetchone() 
+
+# Apply the key to bot
+bot = telepot.Bot(str(results.key))
+
+# Execute SQL command
+SQLCommand = ("select * from conf_data")
+main_cursor.execute(SQLCommand)
+results = main_cursor.fetchone() 
+
+# Store results in a dictionary
+data = {}
+while results:
+    data[str(results.key)] = str(results.value)
+    results = main_cursor.fetchone()
+
+
+
 def subscribe(chat_id):
-    with open("subscribers.txt", "r+") as f:
-        for line in f:
-            if chat_id in line:
-                bot.sendMessage(chat_id, "Already Subscribed!")
-                break
-        else:    
-            f.write(str(chat_id + "\n"))
-            bot.sendMessage(chat_id, "Subscription Successful!")
+    io_connection = pyodbc.connect(connection_string)
+    io_cursor = io_connection.cursor()
+    check_command = ("select * from subscribers where subscriber_id = ? and [status] = 1")
+    param_chat_id = chat_id
+    io_cursor.execute(check_command, param_chat_id)
+    results = io_cursor.fetchone()
+    if(results != None):
+        bot.sendMessage(chat_id, "Already Subscribed!") 
+    else:
+        subscribe_command = ("insert into subscribers (subscriber_id, status, create_datetime) values (?,?,?)")
+        values = [str(chat_id), 1, datetime.datetime.now()]
+        io_cursor.execute(subscribe_command, values)
+        io_connection.commit()
+        bot.sendMessage(chat_id, "Subscription Successful!")
+    io_connection.close() 
 
 
 def unsubscribe(chat_id):
-    f = open("subscribers.txt", "r+")
-    d = f.readlines()
-    f.seek(0)
-    for i in d:
-        if i != chat_id + "\n":
-            f.write(i)
-    f.truncate()
-    f.close()
-    bot.sendMessage(chat_id, "Unsubscribed!")        
-        
+    io_connection = pyodbc.connect(connection_string)
+    io_cursor = io_connection.cursor()
+    check_command = ("select * from subscribers where subscriber_id = ? and [status] = 1")
+    param_chat_id = chat_id
+    io_cursor.execute(check_command, param_chat_id)
+    results = io_cursor.fetchone()
+    if(results == None):
+        bot.sendMessage(chat_id, "Not Subscribed!") 
+    else:
+        subscribe_command = ("update subscribers set [status] = 0 , lastupdate_datetime = ? where subscriber_id = ?")
+        values = [datetime.datetime.now(), chat_id]
+        io_cursor.execute(subscribe_command, values)
+        io_connection.commit()
+        bot.sendMessage(chat_id, "Unsubscribed!") 
+    io_connection.close()
 
 def sendNotification(message):
-    f = open("subscribers.txt", "r")
-    for line in f:
-        bot.sendMessage(line, message)
+    io_connection = pyodbc.connect(connection_string)
+    io_cursor = io_connection.cursor()
+    all_subscribers = ("select * from subscribers where [status] = 1")
+    io_cursor.execute(all_subscribers)
+    results = io_cursor.fetchone()
+    while(results):
+        bot.sendMessage(str(results.subscriber_id), message)
+        results = io_cursor.fetchone()
+    io_connection.close()
 
+# Close the connection
+main_connection.close()
 
 def handle(msg):
     chat_id = msg['chat']['id']
@@ -46,35 +109,36 @@ def handle(msg):
     print('Got command: %s' % command)
 
     if command == '/start':
-        bot.sendMessage(chat_id, "Hi! I'm Jake! I am a half baked AI bot running on Sagar's Work Machine ready to perform some tasks for you. Pick a command from the list and I will try my best!" + "\n1. /roll - Toll a six sided die.\n2. /time - Show the current system time.\n3. /about - Information about the bot.\n4. /crypto - Get current crypto currency exchange rates.")
+        bot.sendMessage(chat_id, str(data['start']).replace("\\n", "\n"))
     elif command == '/roll':
-        bot.sendMessage(chat_id, str(random.randint(1,6)) + "  That was quick wasn't it?")
+        bot.sendMessage(chat_id, str(random.randint(1,6)) + " " + data['roll'])
     elif command == '/time':
-        bot.sendMessage(chat_id, "The time is " + str(time.strftime("%I:%M %p")))
+        bot.sendMessage(chat_id, data['time'] + " " + str(time.strftime("%I:%M %p")))
     elif command == '/about':
-        bot.sendMessage(chat_id, "Hi I'm Jake! I am a half baked AI bot created by Sagar. He is still training me to be more useful. Currently I am running on his Work Machine.")
+        bot.sendMessage(chat_id, data['about'])
     elif command == '/subscribe':
         subscribe(str(chat_id))
     elif command == '/unsubscribe':
         unsubscribe(str(chat_id))
     elif command == '/crypto':
-        url = 'https://api.coinbase.com/v2/prices/USD/spot?'
+        url = data['crypto_url']
         response = get(url).json()
         bot.sendMessage(chat_id, "BTC: $"+response['data'][0]['amount'] + "\n" + "BCH: $"+response['data'][1]['amount'] + "\n" + "ETH: $"+response['data'][2]['amount'] + "\n" + "LTC: $"+response['data'][3]['amount'])
+    elif command == '/formataddress':
+        bot.sendMessage(chat_id, data['address_input'])
+    elif command[:7].lower() == "address":
+        try:
+            geocode_result = gmaps.geocode(msg['text'][1:])
+            bot.sendMessage(chat_id, "Formatted Address: \n" + str(geocode_result[0]['formatted_address']))
+        except:
+            bot.sendMessage(chat_id, data['address_error'])
     else:
-        bot.sendMessage(chat_id, "Oops! I don't seem to understand this command. These are the only commands I can understand: " + "\n1. /roll - Toll a six sided die.\n2. /time - Show the current system time.\n3. /about - Information about the me.\n4. /crypto - Get current crypto currency exchange rates.")
+        bot.sendMessage(chat_id, str(data['error']).replace("\\n", "\n"))
 
-
-keys = {}
-with open('keys.csv', newline='') as keystore:
-    reader = csv.reader(keystore)
-    next(reader)
-    keys = dict(reader)
-
-bot = telepot.Bot(keys['jake'])
 
 MessageLoop(bot, handle).run_as_thread()
 print('I am listening ...')
+
 
 while 1:
     time.sleep(10)
